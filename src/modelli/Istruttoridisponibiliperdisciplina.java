ae8d646b2a1c464908bcf45c4aa0e060aeb85606
Claudio <Claudio@claudio>
2017-03-16 18:29:36 +0100

    dcsc

---
+++ src/modelli/Istruttoridisponibiliperdisciplina.java
@@ -0,0 +1,26 @@
+package modelli;
+
+public class Istruttoridisponibiliperdisciplina {
+private int combinazioneistrdis;
+private Istruttore istruttore;
+private Disciplinedisponibili disciplinedisponibili;
+public int getCombinazioneistrdis() {
+	return combinazioneistrdis;
+}
+public void setCombinazioneistrdis(int combinazioneistrdis) {
+	this.combinazioneistrdis = combinazioneistrdis;
+}
+public Istruttore getIstruttore() {
+	return istruttore;
+}
+public void setIstruttore(Istruttore istruttore) {
+	this.istruttore = istruttore;
+}
+public Disciplinedisponibili getDisciplinedisponibili() {
+	return disciplinedisponibili;
+}
+public void setDisciplinedisponibili(Disciplinedisponibili disciplinedisponibili) {
+	this.disciplinedisponibili = disciplinedisponibili;
+}
+
+}
---
+++ src/modelli/Sceltaorarioperiscrizione.java
@@ -0,0 +1,19 @@
+package modelli;
+
+public class Sceltaorarioperiscrizione {
+	private Fasciaoraria preferenzafascia;
+	private Iscrizionedisciplina iscrizionedisciplina;
+	public Fasciaoraria getPreferenzafascia() {
+		return preferenzafascia;
+	}
+	public void setPreferenzafascia(Fasciaoraria preferenzafascia) {
+		this.preferenzafascia = preferenzafascia;
+	}
+	public Iscrizionedisciplina getIscrizionedisciplina() {
+		return iscrizionedisciplina;
+	}
+	public void setIscrizionedisciplina(Iscrizionedisciplina iscrizionedisciplina) {
+		this.iscrizionedisciplina = iscrizionedisciplina;
+	}
+
+}
